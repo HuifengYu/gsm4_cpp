ef643759685bbb4d8a78255c6fdc6c6f3df7d39f
PeerLess-Swordsman <40665851+PeerLess-Swordsman@users.noreply.github.com>
2020-10-12 21:06:43 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# start sshd
+__run_() {
+  echo -e "123456\n123456" | (passwd)
+  /usr/sbin/sshd -D &
+}
+
+# start node
+__start_pm2() {
+  cd /var/www/Moniti
+  npm i
+  pm2 start index.js --name moniti --node-args='--max-old-space-size=2048'
+  cd /var/www/MoniDev
+  npm i
+  pm2 start index.js --name monidev --node-args='--max-old-space-size=2048'
+}
+
+__start_cpp() {
+  cd /var/www/Forever/bin
+  ./forever
+}
+
+# run
+__start_pm2
+__run_ $1
+__start_cpp
